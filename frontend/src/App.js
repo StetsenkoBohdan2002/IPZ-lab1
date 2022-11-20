86e07bce058f1139af01597a7cb9932c2830fad7
Bohdan <stetsenko.bogdan.work@gmail.com>
2022-11-20 14:54:30 +0200

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className='App'>
+      <h2>IV-93</h2>
+      <ul>
+        <li>Bohdan Stetsenko</li>
+        <li>Dmytryshyn Andriy</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
